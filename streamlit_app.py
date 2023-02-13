89eaa7dfd195add90facc9ae6c8f9c3a5f2ea241
snowtryacct <125313751+snowtryacct@users.noreply.github.com>
2023-02-13 17:49:20 -0500

    Update streamlit_app.py
    emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 streamlit.title('My Parents new Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spanish & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spanish & Rocket Smoothie')
+streamlit.text('🥑Hard-Boiled Free-Range Egg')
