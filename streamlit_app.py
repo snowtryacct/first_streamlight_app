3e2db5561838e128e63e2f1683b12e9cae0d7904
snowtryacct <125313751+snowtryacct@users.noreply.github.com>
2023-02-18 21:08:31 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ def get_fruityvice_data(this_fruit_choice):
 #snowflake related function
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("select * from fruit_load_list")
+    my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
     return my_cur.fetchall()  
 
 streamlit.title('My Parents new Healthy Diner')
